7fcfc5039b60e476dccdc8b538a1b54d52700e5c
Dmitry Kaznacheev <dikaznaceev@gmail.com>
2021-04-03 19:29:22 +1000

    initial commit

---
+++ JsonKnownTypes.Swashbuckle/JsonKnownTypesDataContractResolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Swashbuckle.AspNetCore.Newtonsoft;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace JsonKnownTypes.Swashbuckle
+{
+    public class JsonKnownTypesDataContractResolver : ISerializerDataContractResolver
+    {
+        private readonly NewtonsoftDataContractResolver _resolver;
+        public JsonKnownTypesDataContractResolver(NewtonsoftDataContractResolver resolver)
+        {
+            _resolver = resolver;
+        }
+        
+        public DataContract GetDataContractForType(Type type)
+        {
+            var contract = _resolver.GetDataContractForType(type);
+            
+            if (JsonKnownTypesCache.TypeToDiscriminator.TryGetValue(type, out var fieldName))
+            {
+                var discriminatorProperty = new DataProperty(fieldName, typeof(string));
+
+                var properties = new List<DataProperty>
+                {
+                    discriminatorProperty
+                };
+                
+                properties.AddRange(contract.ObjectProperties.ToList());
+                
+                return DataContract.ForObject(contract.UnderlyingType,
+                    properties,
+                    contract.ObjectExtensionDataType,
+                    contract.ObjectTypeNameProperty,
+                    contract.ObjectTypeNameValue,
+                    contract.JsonConverter);
+            }
+
+            return contract;
+        }
+    }
+}
---
+++ JsonKnownTypes.Swashbuckle/JsonKnownTypesExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Newtonsoft;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+#if (NETSTANDARD2_0)
+using MvcNewtonsoftJsonOptions = Microsoft.AspNetCore.Mvc.MvcJsonOptions;
+#endif
+
+namespace JsonKnownTypes.Swashbuckle
+{
+    public static class JsonKnownTypesExtensions
+    {
+        public static IServiceCollection AddSwaggerGenJsonKnownTypesSupport(this IServiceCollection services)
+        {
+            return services.Replace(
+                ServiceDescriptor.Transient<ISerializerDataContractResolver>(s =>
+                {
+                    var serializerSettings = s.GetRequiredService<IOptions<MvcNewtonsoftJsonOptions>>().Value?.SerializerSettings
+                                             ?? new JsonSerializerSettings();
+
+                    var newtonsoftResolver = new NewtonsoftDataContractResolver(serializerSettings);
+                    return new JsonKnownTypesDataContractResolver(newtonsoftResolver);
+                }));
+        }
+    }
+}
